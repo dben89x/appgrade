399821b6880e0394af246f6ca9a317e38b7aca16
Doug Bennett <dben89x@gmail.com>
2018-04-15 19:28:58 -0700

    adding spinner

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -7,39 +7,63 @@ require('../../styles/style.scss')
 
 export default class App extends React.Component {
 
+  componentWillMount() {}
+
   componentDidMount() {
-    $('#appgradeText h1').animate({'opacity': '1'}, {duration: 1000, queue: false} )
-    // $('#appgradeText h1:nth-child(1)').animate({'right': '50%'}, {duration: 500, queue: false} )
-    // $('#appgradeText h1:nth-child(2)').animate({'left': '50%'}, {duration: 500, queue: false} )
-    $('#appgradeText h1:nth-child(1)').animate({'margin-right': '0'}, {duration: 500, queue: false} )
-    $('#appgradeText h1:nth-child(2)').animate({'margin-left': '0'}, {duration: 500, queue: false} )
+    $('#appgradeText h1').animate({
+      'opacity': '1'
+    }, {
+      duration: 1000,
+      queue: false
+    })
+    $('#appgradeText h1:nth-child(1)').animate({
+      'margin-right': '0'
+    }, {
+      duration: 500,
+      queue: false
+    })
+    $('#appgradeText h1:nth-child(2)').animate({
+      'margin-left': '0'
+    }, {
+      duration: 500,
+      queue: false
+    })
   }
 
   render() {
+    const Spinner = () => (<div class="container">
+      <svg class="loader" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 340 340">
+        <circle cx="170" cy="170" r="160" stroke="#E2007C"/>
+        <circle cx="170" cy="170" r="135" stroke="#404041"/>
+        <circle cx="170" cy="170" r="110" stroke="#E2007C"/>
+        <circle cx="170" cy="170" r="85" stroke="#404041"/>
+      </svg>
+    </div>)
+
     return (<div id='banner1' className='banner'>
+      <Spinner/>
       <span id="appgradeText">
         <div className="headerContainer">
           <h1>app</h1>
           <h1>grade</h1>
         </div>
       </span>
-      {/* <div id='overlay1'>
-    </div> */
-      }
-      {/* <div id='overlay2'>
-    <div className='content'></div>
-    <div className='overlay-top overlay-edges'></div>
-    <div className='overlay-bottom overlay-edges'></div>
-    <div id='polygon1'></div>
-  </div> */
-      }
-      {/* <div className='circle-container'>
-          <div className='circle'>
+
+      {/* <div id='overlay1'></div>
+      <div id='overlay2'>
+        <div className='content'></div>
+        <div className='overlay-top overlay-edges'></div>
+        <div className='overlay-bottom overlay-edges'></div>
+        <div id='polygon1'></div>
+      </div>
+      <div className='circle-container'>
+        <div className='circle'>
           <h1>foo</h1>
           <h2>bar</h2>
         </div>
-        </div> */
+      </div> */
       }
+
     </div>)
   }
 }
